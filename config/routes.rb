084c2fbfdcbd1cbb86e705bf1ec2bd3e64becf72
Arash Kashani <akashani62@gmail.com>
2018-02-16 09:16:49 -0500

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmlend
+
+  root "welcome#index"
+  def query_params_to_query(request)
+    query_params = request.params.except(:path, :format)
+    query_params.any? ? "?#{query_params.to_query}" : ""
+  end
+
+  constraints(host: %r{^arashkashani.com}) do
+    redirect_action = ->(params, _request) do
+      "https://www.arashkashani.com/#{params[:path]}#{query_params_to_query(_request)}"
+    end
+    root to: redirect(redirect_action)
+    get '/*path', to: redirect(redirect_action)
+  end
+
 end
