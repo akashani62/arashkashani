b2aa6bf73bc811097dc2bc1aa9460e11fb34b742
Arash Kashani <akashani62@gmail.com>
2018-02-22 00:22:12 -0500

    more route again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
-  config.ssl_options = { redirect: { host: "secure.arashkashani.com", port: 8080, exclude: -> request { request.path =~ /healthcheck/ } }
+  config.ssl_options = { redirect: { host: "secure.arashkashani.com", port: 8080, exclude: -> request { request.path =~ /healthcheck/ } } }
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
