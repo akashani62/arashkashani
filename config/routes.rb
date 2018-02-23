59295753a88a0486e1e1c7bc1556cdaef5f9e9c8
Arash Kashani <akashani62@gmail.com>
2018-02-23 11:55:00 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   root "welcome#index"
-  # def query_params_to_query(request)
-  #   query_params = request.params.except(:path, :format)
-  #   query_params.any? ? "?#{query_params.to_query}" : ""
-  # end
-  #
-  # constraints(host: %r{^arashkashani.com}) do
-  #   redirect_action = ->(params, _request) do
-  #     "https://www.arashkashani.com/#{params[:path]}#{query_params_to_query(_request)}"
-  #   end
-  #   root to: redirect(redirect_action)
-  #   get '/*path', to: redirect(redirect_action)
-  # end
+
+  def query_params_to_query(request)
+    query_params = request.params.except(:path, :format)
+    query_params.any? ? "?#{query_params.to_query}" : ""
+  end
+
+  constraints(host: %r{^arashkashani.com}) do
+    redirect_action = ->(params, _request) do
+      "https://secure.arashkashani.com/#{params[:path]}#{query_params_to_query(_request)}"
+    end
+    root to: redirect(redirect_action)
+    match '/*path', to: redirect(redirect_action), via: [:get, :post]
+  end
 end
