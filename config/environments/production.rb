2256c1203ae69891b56a82d566d616fe40dfa7bd
Arash Kashani <akashani62@gmail.com>
2018-02-24 01:06:48 -0500

    typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "arashkashani_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  s
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
