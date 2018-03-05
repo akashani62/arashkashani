5155fca5a948d175245d3f65316f196fdbc2c486
Arash Kashani <akashani62@gmail.com>
2018-03-05 08:58:43 -0500

    s

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
-  config.action_controller.asset_host = "cdn.arashkashani.com"
+  config.action_controller.asset_host = "https://cdn.arashkashani.com"
   # config.action_controller.asset_host = "https://www.arashkashani.com"
 
 
