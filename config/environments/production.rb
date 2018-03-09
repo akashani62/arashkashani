7c7c46dc26accb04d4fac1e4a88acb2056d78eab
Arash Kashani <akashani62@gmail.com>
2018-03-09 13:36:11 -0500

    add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
-  config.action_controller.asset_host = 'd1h7io2m1coeep.cloudfront.net'
+  # config.action_controller.asset_host = 'd1h7io2m1coeep.cloudfront.net'
   # config.action_controller.asset_host = 'cdn.arashkashani.com'
 
   # Specifies the header that your server uses for sending files.
