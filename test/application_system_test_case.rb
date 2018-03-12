436677f731569f8730d1e3e26da71ec5e626c18d
Arash Kashani <akashani62@gmail.com>
2018-03-12 13:38:41 -0400

    analyt

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,18 +3,15 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
-  # driven_by :webkit
-
+  # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :webkit
   # driven_by :selenium, using: :headless_chrome # , screen_size: [1400, 1400]
-
   Capybara::Webkit.configure do |config|
     config.allow_url('arashkashani.com')
     config.allow_url('arashkashani.net')
     config.allow_url('www.googletagmanager.com')
     config.allow_url('www.google-analytics.com')
     config.allow_url('js-agent.newrelic.com')
+    config.allow_url('www.google.ca')
   end
-
-  # driven_by :selenium, using: :headless_chrome#, screen_size: [1400, 1400]
 end
