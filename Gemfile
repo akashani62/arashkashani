358458715ab07e1f5a4190919a75592d42342424
Arash Kashani <akashani62@gmail.com>
2018-03-12 18:37:09 -0400

    const

--- Gemfile
+++ Gemfile
@@ -41,9 +41,9 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'awesome_print'
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'pry-remote'
-  gem 'awesome_print'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,13 @@
 Rails.application.routes.draw do
   resources :welcome
   root 'welcome#index'
-  # require 'pry-remote'
-  # binding.remote_pry
-  puts "\n\n\n\n\n\n\n\n\n\n\ngethostnsame::"
-  puts Socket.gethostname
-  puts "end======\n\n\n\n\n\n\n\n\n\n\n:"
-
   def query_params_to_query(request)
     query_params = request.params.except(:path, :format)
     query_params.any? ? "?#{query_params.to_query}" : ''
   end
 
-  constraints(host: /arashkashani\.herokuapp\.com/) do
+  constraints(host: /^herokuapp.arashkashani.com/) do
     redirect_action = lambda do |params, _request|
-
       "https://www.arashkashani.com/#{params[:path]}#{query_params_to_query(_request)}"
     end
     root to: redirect(redirect_action)
