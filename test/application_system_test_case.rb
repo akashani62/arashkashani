fee77bad693b0a414c75634dee65d05087153bc5
Arash Kashani <akashani62@gmail.com>
2018-03-12 09:08:04 -0400

    pushj

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -8,6 +8,8 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   Capybara::Webkit.configure do |config|
     config.allow_url('arashkashani.com')
     config.allow_url('arashkashani.net')
+    config.allow_url('www.googletagmanager.com')
+    config.allow_url('www.google-analytics.com')
   end
 
   # driven_by :selenium, using: :headless_chrome#, screen_size: [1400, 1400]
