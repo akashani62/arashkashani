c7685c01af4704cccc10e307b08d7b6e9239d3d2
Arash Kashani <akashani62@gmail.com>
2018-03-12 18:40:39 -0400

    route

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,16 @@
 Rails.application.routes.draw do
   resources :welcome
   root 'welcome#index'
-  def query_params_to_query(request)
-    query_params = request.params.except(:path, :format)
-    query_params.any? ? "?#{query_params.to_query}" : ''
-  end
-
-  constraints(host: /^herokuapp.arashkashani.com/) do
-    redirect_action = lambda do |params, _request|
-      "https://www.arashkashani.com/#{params[:path]}#{query_params_to_query(_request)}"
-    end
-    root to: redirect(redirect_action)
-    match '/*path', to: redirect(redirect_action), via: %i[get post]
-  end
+  # def query_params_to_query(request)
+  #    query_params = request.params.except(:path, :format)
+  #    query_params.any? ? "?#{query_params.to_query}" : ''
+  #  end
+  #
+  #  constraints(host: /^herokuapp.arashkashani.com/) do
+  #    redirect_action = lambda do |params, _request|
+  #      "https://www.arashkashani.com/#{params[:path]}#{query_params_to_query(_request)}"
+  #    end
+  #    root to: redirect(redirect_action)
+  #    match '/*path', to: redirect(redirect_action), via: %i[get post]
+  #  end
 end
